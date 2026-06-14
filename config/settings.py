88a8be403332f89110cdfba8086e36ab407adf97
Denys Peresunko <peresunkodenis11@gmail.com>
2026-06-14 17:40:13 +0200

    Add homepage with cream/cognac design system and base layout

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files (загруженные пользователями)
 MEDIA_URL = '/media/'
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def home(request):
+    """Главная страница."""
+    # Берём 6 последних активных товаров для секции "Останні роботи"
+    latest_products = Product.objects.filter(is_active=True)[:6]
+
+    context = {
+        'latest_products': latest_products,
+    }
+    return render(request, 'shop/home.html', context)
